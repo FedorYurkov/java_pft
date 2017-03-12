524ec68c51b00c732c2edc2fd151cbf7cdb192ab
Fedor <fedoryurkov@gmail.com>
2017-03-12 11:27:03 +0300

    Создан класс Point, запускаемый класс Distance
    Вычисление по функции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+public class Distance {
+
+  public static void main(String[] args){
+
+    Point p1 = new Point(0,0);
+    Point p2 = new Point(3,4);
+
+    System.out.println("Расстояние между точками p1(" + p1.x + ", " + p1.y + ") и p2(" + p2.x + ", " + p2.y + ") равно " + distance(p1, p2));
+
+  }
+
+  public static double distance(Point p1, Point p2){
+    double deltaX = p1.x - p2.x;
+    double deltaY = p1.y - p2.y;
+    double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+    return distance;
+  }
+}
