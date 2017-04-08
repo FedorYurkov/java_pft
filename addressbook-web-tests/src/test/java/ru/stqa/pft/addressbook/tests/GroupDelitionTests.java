ff06add3660d9e7184e0427be09967cad20f1d35
Fedor <fedoryurkov@gmail.com>
2017-04-08 10:02:50 +0300

    Для групп реализованы проверки в fluent-стиле, используя Hamcrest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTests.java
@@ -1,11 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupDelitionTests extends TestBase {
 
@@ -21,17 +23,16 @@ public class GroupDelitionTests extends TestBase {
 
   @Test
   public void testGroupDelition() {
+    Groups before = app.group().all();
 
-    Set<GroupData> before = app.group().all();
     GroupData deletedGroup = before.iterator().next();
 
     app.group().delete(deletedGroup);
+    Groups after = app.group().all();
 
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(), before.size() - 1);
+    assertEquals(after.size(), before.size() - 1);
+    assertThat(after, equalTo(before.withOut(deletedGroup)));
 
-    before.remove(deletedGroup);
-    Assert.assertEquals(before, after);
   }
 
 
