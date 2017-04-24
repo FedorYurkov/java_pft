011412e29884493cd2d408b5b6e53498f314805b
Fedor <fedoryurkov@gmail.com>
2017-04-24 21:58:28 +0300

    testHbConnection переделан для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -2,49 +2,91 @@ package ru.stqa.pft.addressbook.model;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
 @XStreamAlias("contact")
+@Entity
+@Table (name = "addressbook")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
+  @Column(name = "firstname")
   private String firstName;
+  @Column(name = "middlename")
   private String midName;
+  @Column(name = "lastname")
   private String lastName;
+  @Column(name = "nickname")
   private String nick;
+  @Column(name = "title")
   private String title;
+  @Column(name = "company")
   private String company;
+  @Column(name = "address")
+  @Type(type = "text")
   private String address;
 
+  @Column(name = "home")
+  @Type(type = "text")
   private String homePhone;
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
+  @Column(name = "work")
+  @Type(type = "text")
   private String workPhone;
+  @Transient
   private String allPhones;
 
+  @Column(name = "fax")
+  @Type(type = "text")
   private String fax;
 
+  @Column(name = "email")
+  @Type(type = "text")
   private String email;
+  @Column(name = "email2")
+  @Type(type = "text")
   private String email2;
+  @Column(name = "email3")
+  @Type(type = "text")
   private String email3;
+  @Transient
   private String allEmails;
 
+  @Column(name = "homepage")
+  @Type(type = "text")
   private String site;
+  @Transient
   private String group;
+  @Column(name = "address2")
+  @Type(type = "text")
   private String secondaryAddress;
+  @Column (name = "phone2")
+  @Type(type = "text")
   private String home;
+  @Column(name = "notes")
+  @Type(type = "text")
   private String notes;
 
+  @Transient
   private String detailsInfo;
 
-  private File photo;
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
