19503819fa90b02d30940a4a5e4cf3fc7198e6c9
Fedor <fedoryurkov@gmail.com>
2017-05-13 01:23:30 +0300

    Пример работы с GitHub через Remote API

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GitHubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GitHubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("4c41ee879cd740a1b554bc6068569db4f3bb5bba");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("FedorYurkov", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
